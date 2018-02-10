df32f02a4e4d6bd6d7876551a31bef55ad158a99
Reuel <2121855@slu.edu.ph>
2018-02-10 20:49:42 +0800

    addition of pages for selection of items with other edits

---
+++ GoldustCreations/itemSelectionNav.php
@@ -0,0 +1,24 @@
+
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-centered">
+              <li><a href="gowns.php">Gowns</a></li>
+              <li><a href="suits.php">Suits</a></li>
+              <li><a href="costumes.php">Costumes</a></li>
+              <li><a href="decors.php">Decors</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
