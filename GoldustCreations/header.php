5b843dd8130d4313173613385ac1ec364a8a2d2b
zerocokes <32257743+zerocokes@users.noreply.github.com>
2018-02-11 01:33:20 +0800

    update header timezone
    timezone not complete

--- GoldustCreations/header.php
+++ GoldustCreations/header.php
@@ -17,61 +17,84 @@
       </a>
       <!-- Navbar Right Menu -->
       <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          <!-- Notifications Menu -->
-          <li class="dropdown notifications-menu">
-            <!-- Menu toggle button -->
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-bell-o"></i>
-              <span class="label label-warning">10</span>
+        <!-- Timezone Beginning -->
+        <div class="timezone">
+          <script>
+            function startTime() {
+              var today = new Date();
+              var h = today.getHours();
+              var m = today.getMinutes();
+              var s = today.getSeconds();
+              m = checkTime(m);
+              s = checkTime(s);
+              document.getElementById('txt').innerHTML =
+              h + ":" + m + ":" + s;
+              var t = setTimeout(startTime, 500);
+            }
+            function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+</script>
+<body onload="startTime()">
+  <div id="txt"></div>
+  <!-- End of time Zone -->
+</div>
+<ul class="nav navbar-nav">
+  <!-- Notifications Menu -->
+  <li class="dropdown notifications-menu">
+    <!-- Menu toggle button -->
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+      <i class="fa fa-bell-o"></i>
+      <span class="label label-warning">10</span>
+    </a>
+    <ul class="dropdown-menu">
+      <li class="header">You have 10 notifications</li>
+      <li>
+        <!-- Inner Menu: contains the notifications -->
+        <ul class="menu">
+          <li><!-- start notification -->
+            <a href="#">
+              <i class="fa fa-users text-aqua"></i> 5 new members joined today
             </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 10 notifications</li>
-              <li>
-                <!-- Inner Menu: contains the notifications -->
-                <ul class="menu">
-                  <li><!-- start notification -->
-                    <a href="#">
-                      <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                    </a>
-                  </li>
-                  <!-- end notification -->
-                </ul>
-              </li>
-              <li class="footer"><a href="#">View all</a></li>
-            </ul>
           </li>
-          <!-- User Account Menu -->
-          <li class="dropdown user user-menu">
-            <!-- Menu Toggle Button -->
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <!-- The user image in the navbar-->
-              <img src="dist/img/user1-128x128.jpg" class="user-image" alt="User Image">
-              <!-- hidden-xs hides the username on small devices so only the image appears. -->
-              <span class="hidden-xs">User</span>
-            </a>
-            <ul class="dropdown-menu">
-              <!-- The user image in the menu -->
-              <li class="user-header">
-                <img src="dist/img/user1-128x128.jpg" class="img-circle" alt="User Image">
-
-                <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
-                </p>
-              </li>
-              <!-- Menu Footer-->
-              <li class="user-footer">
-                <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
-                </div>
-                <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
-                </div>
-              </li>
-            </ul>
-          </li>
+          <!-- end notification -->
         </ul>
-      </div>
-    </nav>
-  </header>
+      </li>
+      <li class="footer"><a href="#">View all</a></li>
+    </ul>
+  </li>
+  <!-- User Account Menu -->
+  <li class="dropdown user user-menu">
+    <!-- Menu Toggle Button -->
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+      <!-- The user image in the navbar-->
+      <img src="dist/img/user1-128x128.jpg" class="user-image" alt="User Image">
+      <!-- hidden-xs hides the username on small devices so only the image appears. -->
+      <span class="hidden-xs">User</span>
+    </a>
+    <ul class="dropdown-menu">
+      <!-- The user image in the menu -->
+      <li class="user-header">
+        <img src="dist/img/user1-128x128.jpg" class="img-circle" alt="User Image">
+
+        <p>
+          Alexander Pierce - Web Developer
+          <small>Member since Nov. 2012</small>
+        </p>
+      </li>
+      <!-- Menu Footer-->
+      <li class="user-footer">
+        <div class="pull-left">
+          <a href="#" class="btn btn-default btn-flat">Profile</a>
+        </div>
+        <div class="pull-right">
+          <a href="#" class="btn btn-default btn-flat">Sign out</a>
+        </div>
+      </li>
+    </ul>
+  </li>
+</ul>
+</div>
+</nav>
+</header>
