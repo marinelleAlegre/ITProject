350f2011da7d927b7271158b40c687119f75e512
Reuel <2121855@slu.edu.ph>
2018-02-11 03:03:41 +0800

    not relevant commit

--- GoldustCreations/header.php
+++ GoldustCreations/header.php
@@ -32,10 +32,10 @@
               var t = setTimeout(startTime, 500);
             }
             function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
-</script>
+              if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+              return i;
+            }
+          </script>
 <body onload="startTime()">
   <div id="txt"></div>
   <!-- End of time Zone -->
