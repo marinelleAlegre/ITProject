243a668cdb4ff1692a2d286bce9f22c6ca4b3888
Reuel <2121855@slu.edu.ph>
2018-02-14 16:12:44 +0800

    edit

--- GoldustCreations/header.php
+++ GoldustCreations/header.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-static-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="#" class="navbar-brand"><b>Goldust Creations</a>
+          <a href="#" class="navbar-brand"><b>Goldust Creations</b></a>
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
             <i class="fa fa-bars"></i>
           </button>
