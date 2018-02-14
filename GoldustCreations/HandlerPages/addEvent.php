609ac96c928c5b65d6effc3b2b730e223c26c2e9
maricris-macario <crismaca008@gmail.com>
2018-02-14 16:14:43 +0800

    Edited addEvent.php

--- GoldustCreations/HandlerPages/addEvent.php
+++ GoldustCreations/HandlerPages/addEvent.php
@@ -44,16 +44,7 @@ desired effect
 
     <!-- Main content -->
     <section class="content container-fluid">
-      <ul class="nav nav-tabs">
-        <li><a href="#details" data-toggle="tab">Details</a></li>
-        <li><a href="#" data-toggle="tab">Entourage</a></li>
-        <li><a href="#" data-toggle="tab">Decors</a></li>
-        <li><a href="#" data-toggle="tab">Payment</a></li>
-        <li><a href="#" data-toggle="tab">Expenses</a></li>
-      </ul>
-
-      <div class="tab-content">
-        <div id="details" class="tab-pane">
+          <!-- .... Event Details .... -->
           <form>
             <div class="row">
               <div class="col-lg-4">
@@ -145,52 +136,217 @@ desired effect
                         </div>
                       </form>
                     </td> 
-                    <td>7</td>
-                    <td>10000</td> 
+                    <td><input type="text" id=""></td>
+                    <td><input type="text" name=""></td> 
                   </tbody>
                 </table>
               </div>
               <div class="table-responsive col-lg-6">
-                <table id="" class="table table-striped table-bordered">
+                <table id="staff" class="table table-striped table-bordered">
                   <thead>
                     <tr>
                       <th>Name</th>
                       <th>Role</th>
                       <th>Contact Number</th>
                       <th>Availability</th>
+                      <th>Action</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td>Nemo Kamo</td>
-                      <td>Makeup Artist</td>
-                      <td>09876541234</td>
+                      <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Insert text here"></td>
+                      <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Insert text here"></td>
+                      <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Insert text here"></td>
                       <td>Available</td>
+                      <td><a data-toggle="modal" data-target="#edit-staff"><span class="glyphicon glyphicon-pencil"></span></a></td>
+
+                      <!-- edit staff modal -->
+                      <div id="edit-staff" class="modal fade" role="dialog">
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <button type="button" class="close" data-dismiss="modal" aria-label="close"><span aria-hidden="true">&times;</span></button>
+                              <h4 class="modal-title">Edit Staff</h4>
+                            </div>
+                            <div class="modal-body">
+                              <form>
+                                <div class="row">
+                                  <div class="col-lg-7">
+                                    <div class="form-group">
+                                      <label>Staff Name</label>
+                                      <input type="text" id="staff-name" class="form-control">
+                                    </div>
+                                  </div>
+                                  <div class="col-lg-5">
+                                    <div class="form-group">
+                                      <label>Role</label>
+                                      <!-- combobox intended here.... -->
+                                    </div>
+                                  </div>
+                                </div>
+                              </form>
+                            </div>
+                            <div class="modal-footer">
+                              <button type="submit" class="btn btn-success">Confirm</button>
+                              <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </tr>
                   </tbody>
                   <tfoot>
                     <tr>
-                      <td><button type="button" class="btn btn-primary">Add</button></td>
+                      <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add-staff">Add Staff</button></td>
+                      
+                      <!-- add staff modal -->
+                      <div id="add-staff" class="modal fade">
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h4 class="modal-title">Add Staff</h4>
+                            </div>
+                            <div class="modal-body">
+                              
+                            </div>
+                            <div class="modal-footer">
+                              <button type="submit" class="btn btn-success">Confirm</button>
+                              <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </tr>
                   </tfoot>
                 </table>
               </div>
             </div>
-          </form>
-        </div>
-        <div id="entourage" class="tab-pane">
-          <p>yo</p>
-        </div>
-        <div id="" class="">
-          
-        </div>
-        <div id="" class="">
-          
-        </div>
-        <div id="" class="">
-          
-        </div>
-      </div>
+            <div class="row">
+              <div class="col-lg-1 col-lg-offset-10">
+                <button type="submit" class="btn btn-success">Save</button>
+              </div>
+              <div class="col-lg-1">
+                <button type="reset" class="btn btn-danger">Reset</button>
+              </div>
+            </div>
+          </form>       
+
+          <!-- Entourage Design -->
+          <div class="box table-responsive">
+            <div class="box-header">
+              <h3 class="box-title">Entourage Designs</h3>
+            </div>
+            <div class="box-body">
+              <table id="ent-designs" class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Design Name</th>
+                    <th>Quantity</th>
+                    <th>Photo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <input class="form-control" type="text" id="" style="border: none;" placeholder="Insert text here">
+                    </td>
+                    <td>
+                      <input class="form-control" type="text" id="" style="border: none;" placeholder="Insert text here">
+                    </td>
+                    <td><input type="file" id="design-pic" accept="image/*"></td>
+                  </tr>
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <td><button type="button" class="btn btn-primary">Add Design</button></td>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+          </div>
+
+          <!-- Entourage -->
+          <div class="box table-responsive">
+            <div class="box-header">
+              <h3 class="box-title">Entourage</h3>
+            </div>
+            <div class="box-body">
+              <table id="ent" class="table table-bordered table-striped table-condensed">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Shoulder</th>
+                    <th>Chest</th>
+                    <th>Stomach</th>
+                    <th>Waist</th>
+                    <th>Arm Length</th>
+                    <th>Arm Hole</th>
+                    <th>Muscle</th>
+                    <th>Pants Length</th>
+                    <th>Baston</th>
+                    <th>Design Photo</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Name"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Role"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Shoulder"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Chest"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Stomach"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Waist"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Arm Length"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Arm Hole"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Muscle"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Pants Length"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Baston"></td>
+                    <td><input type="file" id="design-pic" accept="image/*"></td>
+                    <td><a><span class="glyphicon glyphicon-remove" style="margin: 2px;"></span></a><a><span class="glyphicon glyphicon-pencil" style="margin: 2px;"></span></a></td>
+                  </tr>
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <td><button type="button" class="btn btn-primary">Add Entourage</button></td>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+          </div>
+
+          <!-- Decors -->
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Decors</h3>
+            </div>
+            <div class="box-body table-responsive">
+              <table id="ent" class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Event Name</th>
+                    <th>Equipment Name</th>
+                    <th>Quantity</th>
+                    <th>Photo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Insert text here"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Insert text here"></td>
+                    <td><input class="form-control" type="text" id="" style="border: none;" placeholder="Insert text here"></td>
+                    <td><input type="file" id="design-pic" accept="image/*"></td>
+                  </tr>
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <td><button type="button" class="btn btn-primary">Add Decoration</button></td>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+          </div>
+      
     </section>
     <!-- /.content -->
   </div>
