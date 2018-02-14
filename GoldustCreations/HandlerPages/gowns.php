774093117dabb51c436efa151153ac1c8c45d41b
Arakolin <aracoleen1701@yahoo.com>
2018-02-14 14:24:57 +0800

    Edit and clean up

--- GoldustCreations/HandlerPages/gowns.php
+++ GoldustCreations/HandlerPages/gowns.php
@@ -7,7 +7,35 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <title>Events | Gowns</title>
   <?php include("../head.php") ?>
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<style type="text/css">
+  body {
+  padding-top: 54px;
+}
+
+@media (min-width: 992px) {
+  body {
+    padding-top: 56px;
+  }
+}
+ #img {
+    width: 280px;
+    height: 250px;
+    padding: 10px 10px 10px 10px; !important
+ }
+
+ #text {
+  text-align: left;
+  size: 10px;
+ }
+#con {
+padding: 0 0 0 0;
+}
+
+
+
+</style>
 <!--
 BODY TAG OPTIONS:
 =================
@@ -46,7 +74,73 @@ desired effect
     <a href="addEvent.php" class="btn btn-block btn-primary btn-lg">Add Gowns</a>
       <?php include("../itemSelectionNav.php"); ?>
           
-      <?php include("../gallerygowns.php") ?>
+      <div class="container" id="con">
+      <div class="row text-center text-lg-left">
+
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-xs-6" id="img">
+          <a href="#" class="d-block mb-4 h-100" id="con1">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+          </a>
+        </div>
+      </div>
+
+
+    </div>
       <!--------------------------
         | Your Page Content Here |
         -------------------------->
