e100276d49051f153949f6e6b4752fd9c70fd623
Arakolin <aracoleen1701@yahoo.com>
2018-02-15 19:36:08 +0800

    added adminAdmin.php adminHandler.php adminStaff.php EmployeeNav.php

---
+++ GoldustCreations/EmployeeNav.php
@@ -0,0 +1,34 @@
+<html>
+<style type="text/css">
+  a:hover { 
+    background-color: grey;
+}
+  a:active {
+    color: yellow;
+}
+</style>
+<body>
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-centered">
+              <li><a href="adminHandler.php">Handler</a></li>
+              <li><a href="adminStaff.php">Staff</a></li>
+              <li><a href="adminAdmin.php">Admin</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+</body>
+</html>
