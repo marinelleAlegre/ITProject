abdac2a7c1e561e30783fb8ec5258e53f95faedf
marinelleAlegre <biku.tomoshimasen@gmail.com>
2018-02-21 20:32:37 +0800

    Fixed login page

--- GoldustCreations/login/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="refresh" content="0; url=home.php" />
-  </head>
-  
-  <body>
-  </body>
-</html>
--- GoldustCreations/login/login1.php
+++ GoldustCreations/login/login1.php
@@ -16,12 +16,12 @@
         $password = mysqli_real_escape_string($con,$password);
         
     //Checking is user existing in the database or not
-       $query = "SELECT * FROM 'employees' WHERE username='$username' AND password='$password'";
+       $query = "SELECT * FROM `employees` WHERE username='$username' AND password='$password'";
             $result = mysqli_query($con, $query) or die(mysql_error());
             $rows = mysqli_num_rows($result);
         if($rows == 1){
             $_SESSION['username'] = $username;
-            header("Location: index.php"); // Redirect user to index.php
+            header("Location: ../handlerpages/home.php"); // Redirect user to index.php
             }else{
                 echo "<div class='form'><h3>Username/password is incorrect.</h3>
                 <br/>Click here to <a href='login1.php'>Login</a></div>";
